52c8d400b10762506f64ab743116e32358586d21
uservenkatesh <veddavenkatesh09@gmail.com>
2023-09-14 17:27:32 +0530

    first comit

---
+++ ArmStrongNum.java
@@ -0,0 +1,23 @@
+package pack1;
+
+import java.util.Scanner;
+
+public class ArmStrongNum {
+
+	public static void main(String[] args) {
+		
+		System.out.println("enter the number: ");
+		
+		Scanner sc = new Scanner(System.in); 
+		
+		String a=sc.next();
+		
+	    char[] arr=a.toCharArray();
+	    
+	    
+		
+		
+		
+	}
+
+}
